66b00ba9676979085b3ea4e04d677ff80739e1f8
Neil Lunavat <neillunavat3192@gmail.com>
2025-03-31 00:16:45 +0530

    extras

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,24 @@ import ProductSection from "./components/sections/ProductSection";
 import InstagramSection from "./components/sections/InstagramSection";
 import ContactSection from "./components/sections/ContactSection";
 // import FaqSection from "./components/sections/FaqSection";
+import ClickSpark from "./components/ui/Click";
 
 export default function Home() {
     return (
-        <>
+        <ClickSpark
+            sparkColor="#a2b56d"
+            sparkSize={14}
+            sparkRadius={30}
+            sparkCount={12}
+            duration={500}
+            extraScale={0.8}
+        >
             <HeroSection />
             <ProductSection />
             <AboutSection />
             <InstagramSection />
             {/* <FaqSection /> */}
             <ContactSection />
-        </>
+        </ClickSpark>
     );
 }
